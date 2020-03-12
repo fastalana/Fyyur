1da5678cd76b8a81dc8ba18d60aaea3e98166767
Alana Bellucci <fastalana@gmail.com>
2020-03-12 17:36:52 -0600

    Updated /artists/create route to allow for creation of new artists. This iniital commit only includes fields that were included on the original new_artist.html file.

--- app.py
+++ app.py
@@ -448,15 +448,42 @@ def create_artist_form():
   return render_template('forms/new_artist.html', form=form)
 
 @app.route('/artists/create', methods=['POST'])
-def create_artist_submission():
-  # called upon submitting the new artist listing form
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
+# def create_artist_submission():
+#   # called upon submitting the new artist listing form
+#   # TODO: insert form data as a new Venue record in the db, instead
+#   # TODO: modify data to be the data object returned from db insertion
 
-  # on successful db insert, flash success
-  flash('Artist ' + request.form['name'] + ' was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
+#   # on successful db insert, flash success
+#   flash('Artist ' + request.form['name'] + ' was successfully listed!')
+#   # TODO: on unsuccessful db insert, flash an error instead.
+#   # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
+#   return render_template('pages/home.html')
+
+def create_artist_submission():
+  # TODO: insert form data as a new Artist record in the db, instead - DONE
+  # TODO: modify data to be the data object returned from db insertion - DONE
+  error = False
+  try:
+    new_artist = Artist(
+      name = request.form['name'],
+      city = request.form['city'],
+      state = request.form['state'],
+      phone = request.form['phone'],
+      genres = request.form['genres'],
+      facebook_link = request.form['facebook_link'],
+    )
+    db.session.add(new_artist)
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+    if error:
+      # TODO: on unsuccessful db insert, flash an error instead. - DONE
+      flash('An error occurred. Artist ' + new_artist.name + ' could not be listed.')
+    else:
+      # TODO on successful db insert, flash success - DONE
+      flash('Venue ' + request.form['name'] + ' was successfully listed!')
   return render_template('pages/home.html')
 
 
